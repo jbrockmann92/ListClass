a26110bbd6ebe5f3053d0f749182a1dc77d2b1c5
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-01-27 12:49:04 -0800

    Wrote one test for indexer and the indexer itself. May need to add more tests to make sure indexer is working right.

--- ListTests/IndexTests.cs
+++ ListTests/IndexTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
 
 namespace ListTests
 {
@@ -7,8 +8,21 @@ namespace ListTests
     public class IndexTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestIndexerRightLocation()
         {
+            //Arrange
+            MyList<int> myList = new MyList<int>();
+            int expectedResult = 5;
+            int actualResult;
+
+            //Act
+            myList.Add(3);
+            myList.Add(5);
+            myList.Add(7);
+            actualResult = myList[1];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
